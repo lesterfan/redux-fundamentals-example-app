c2175065afc1c14dd1382d8a4614a486b05a8a1d
Lester Fan <hiiamlife@gmail.com>
2021-07-06 21:46:25 -0400

    Initial progress -- I'm stuck now

--- src/App.js
+++ src/App.js
@@ -1,19 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { createStore } from 'redux'
+
+const initialState = {
+  tasks: [],
+}
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case 'tasks/add':
+      console.log(`Adding ${action.payload}`)
+      return {
+        tasks: [
+          ...state.tasks,
+          { desc: action.payload, complete: false, tags: [] },
+        ],
+      }
+    default:
+      return state
+  }
+}
+const reduxStore = createStore(reducer)
+reduxStore.subscribe(() => {
+  console.log('Rendering!')
+})
 
 function App() {
+  const [todoItem, setTodoItem] = useState('')
   return (
     <div className="App">
       <nav>
         <section>
           <h1>Redux Fundamentals Example</h1>
-
-          <div className="navContent">
-            <div className="navLinks"></div>
-          </div>
         </section>
       </nav>
       <section>
-        <h2>Welcome to the Redux Fundamentals example app!</h2>
+        <h2>Todos</h2>
+      </section>
+      <section>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault()
+            console.log(`Submitted form with value ${todoItem}`)
+            // Add this task to redux here
+            reduxStore.dispatch({ type: 'tasks/add', payload: todoItem })
+          }}
+        >
+          <label>What needs to be done?</label>
+          <input
+            type="text"
+            onChange={(e) => {
+              setTodoItem(e.target.value)
+            }}
+          ></input>
+        </form>
       </section>
     </div>
   )
